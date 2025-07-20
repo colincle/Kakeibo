0839ae4cbbeed02fa4f0e9980407d7c3e0687f3b
Clement Colin <clmnt.co@gmail.com>
2025-07-21 07:21:05 +0900

    Project folders setup

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QtCore/QObject>
+#include <QShortcut>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	QWidget window;
+	window.setAttribute(Qt::WA_StyledBackground, true);
+	window.setAutoFillBackground(true);
+	window.setStyleSheet("background-color: #f5f5f5;");
+	window.setWindowTitle("Kakeibo");
+
+	QShortcut* closeShortcut = new QShortcut(QKeySequence("Ctrl+W"), &window);
+	QObject::connect(closeShortcut, &QShortcut::activated, &window, &QWidget::close);
+
+	QHBoxLayout* mainLayout = new QHBoxLayout(&window);
+	mainLayout->setContentsMargins(0, 0, 0, 0);
+	mainLayout->setSpacing(0);
+
+	LeftPanel* leftPanel = new LeftPanel(&window);
+	RightPanel* rightPanel = new RightPanel(&window);
+
+	QObject::connect(leftPanel, &LeftPanel::pageSelected, rightPanel, &RightPanel::setStatus);
+
+	mainLayout->addWidget(leftPanel, 3);
+	mainLayout->addWidget(rightPanel, 7);
+
+	window.resize(1280, 720);
+	window.setMinimumSize(840, 420);
+	window.show();
+	return app.exec();
+}
